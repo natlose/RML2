e1b3d06e81f8200e35cb15352931f1f80bd1119f
natlose <natlose@gmail.com>
2019-05-14 23:06:06 +0200

    repo init

---
+++ RML_EF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RML_EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
