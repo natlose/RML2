c8de6e24ce5200f1fdbcbeb3e0a6d7b6be0bbbd2
natlose <natlose@gmail.com>
2019-05-15 18:35:12 +0200

    select

--- RML_EF/Program.cs
+++ RML_EF/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace RML_EF
 {
@@ -6,14 +9,46 @@ namespace RML_EF
     {
         static void Main(string[] args)
         {
-            using (Entities entites = new Entities())
-            {
-                Person person = new Person { FirstName = "Jenő", LastName = "Nagylaki" };
-                person.Addresses.Add(new Address { Kind = AddressKind.MAIL, Country3166 = "HU", Zip = "1043", City = "Budapest", Line1 = "Váci út 103." });
-                entites.Persons.Add(person);
+            #region INSERT
+            //using (Entities entites = new Entities())
+            //{
+            //    Person person = new Person { FirstName = "Jenő", LastName = "Nagylaki" };
+            //    person.Addresses.Add(new Address { Kind = AddressKind.MAIL, Country3166 = "HU", Zip = "1043", City = "Budapest", Line1 = "Váci út 103." });
+            //    entites.Persons.Add(person);
 
-                entites.SaveChanges();
+            //    entites.SaveChanges();
+            //}
+            #endregion
+
+            #region SELECT
+            using (Entities entities = new Entities())
+            {
+                // Ezekhez kell a using System.Linq
+                Console.WriteLine("LINQ Expressions:");
+                var result = from p in entities.Persons
+                             where p.FirstName == "Ferenc"
+                             select new { Telefon = p.Phone, Nev = p.LastName + " " + p.FirstName };
+                // Lépésenkénti Debug módban a Locals ablakban ezen a ponton látszik, hány lépésből építette fel a fenti Expression objektumgráfot!
+                foreach (var item in result.ToArray()) Console.WriteLine("{0,-60} {1,-30}", item.Nev, item.Telefon);
+
+                // Ez ugyanaz, mint a fenti, csak nem LINQ Expressions, hanem LINQ Extensions technikával
+                Console.WriteLine("\nLINQ Extensions:");
+                foreach (var item in entities.Persons.Where(p => p.FirstName == "Ferenc").Select(p => new { Telefon = p.Phone, Nev = p.LastName + " " + p.FirstName }).ToArray())
+                    Console.WriteLine("{0,-60} {1,-30}", item.Nev, item.Telefon);
+
+                // Alapból nem tölti be a függő egyedeket, az Addresses üres marad!
+                Console.WriteLine("\nLazy loading:");
+                var result2 = from p in entities.Persons select p;
+                foreach (var item in result2.ToArray()) Console.WriteLine("{0,-30} {1,-30} {2,2}", item.FirstName, item.LastName, item.Addresses.Count);
+
+                Console.WriteLine("\nEager loading:");
+                // Lehet kérni a függő egyedek betöltését
+                var result3 = from p in entities.Persons.Include(p => p.Addresses) select p;
+                foreach (var item in result3.ToArray()) Console.WriteLine("{0,-30} {1,-30} {2,2} {3,-20}", item.FirstName, item.LastName, item.Addresses.Count, item.Addresses.FirstOrDefault()?.City);
             }
+            #endregion
+
+
         }
     }
 }
