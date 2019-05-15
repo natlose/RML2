579064d40b8e34aef3a19449d776db5fee68f3bf
natlose <natlose@gmail.com>
2019-05-15 16:27:41 +0200

    entity classes added

---
+++ RML_EF/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RML_EF
+{
+    public enum AddressKind { RES, MAIL, DEL }
+
+    public class Address
+    {
+        public int Id { get; set; }
+        public AddressKind Kind { get; set; }
+        public string Country3166 { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+
+        public int FI_Person { get; set; }
+        public Person Person { get; set; }
+    }
+}
