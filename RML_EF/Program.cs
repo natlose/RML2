4a5f90333e981c15c286e58005e545334e173c28
natlose <natlose@gmail.com>
2019-05-15 23:44:06 +0200

    CRUD

--- RML_EF/Program.cs
+++ RML_EF/Program.cs
@@ -9,46 +9,35 @@ namespace RML_EF
     {
         static void Main(string[] args)
         {
-            #region INSERT
-            //using (Entities entites = new Entities())
-            //{
-            //    Person person = new Person { FirstName = "Jenő", LastName = "Nagylaki" };
-            //    person.Addresses.Add(new Address { Kind = AddressKind.MAIL, Country3166 = "HU", Zip = "1043", City = "Budapest", Line1 = "Váci út 103." });
-            //    entites.Persons.Add(person);
-
-            //    entites.SaveChanges();
-            //}
-            #endregion
-
-            #region SELECT
+            int ArankaId;
+            
+            // Lépésenkénti végrehajtással!
             using (Entities entities = new Entities())
             {
-                // Ezekhez kell a using System.Linq
-                Console.WriteLine("LINQ Expressions:");
-                var result = from p in entities.Persons
-                             where p.FirstName == "Ferenc"
-                             select new { Telefon = p.Phone, Nev = p.LastName + " " + p.FirstName };
-                // Lépésenkénti Debug módban a Locals ablakban ezen a ponton látszik, hány lépésből építette fel a fenti Expression objektumgráfot!
-                foreach (var item in result.ToArray()) Console.WriteLine("{0,-60} {1,-30}", item.Nev, item.Telefon);
-
-                // Ez ugyanaz, mint a fenti, csak nem LINQ Expressions, hanem LINQ Extensions technikával
-                Console.WriteLine("\nLINQ Extensions:");
-                foreach (var item in entities.Persons.Where(p => p.FirstName == "Ferenc").Select(p => new { Telefon = p.Phone, Nev = p.LastName + " " + p.FirstName }).ToArray())
-                    Console.WriteLine("{0,-60} {1,-30}", item.Nev, item.Telefon);
-
-                // Alapból nem tölti be a függő egyedeket, az Addresses üres marad!
-                Console.WriteLine("\nLazy loading:");
-                var result2 = from p in entities.Persons select p;
-                foreach (var item in result2.ToArray()) Console.WriteLine("{0,-30} {1,-30} {2,2}", item.FirstName, item.LastName, item.Addresses.Count);
-
-                Console.WriteLine("\nEager loading:");
-                // Lehet kérni a függő egyedek betöltését
-                var result3 = from p in entities.Persons.Include(p => p.Addresses) select p;
-                foreach (var item in result3.ToArray()) Console.WriteLine("{0,-30} {1,-30} {2,2} {3,-20}", item.FirstName, item.LastName, item.Addresses.Count, item.Addresses.FirstOrDefault()?.City);
+                Person person = new Person { FirstName = "Aranka", LastName = "Soproni" };
+                person.Addresses.Add(new Address { Kind = AddressKind.DEL, Country3166 = "HU", Zip = "8000", City = "Székesfehérvár", Line1 = "Budai út 6." });
+                entities.Persons.Add(person);
+                entities.SaveChanges();
+                ArankaId = person.Id;
             }
-            #endregion
-
-
+            // Itt nézd meg az adatbázist!
+            using (Entities entities = new Entities())
+            {
+                Person aranka = entities.Persons.Include(p => p.Addresses).Single(p => p.Id == ArankaId);
+                aranka.Phone = "+363011223344";
+                aranka.Addresses.Single(a => a.Kind == AddressKind.DEL).Kind = AddressKind.MAIL;
+                entities.SaveChanges();
+            }
+            // Itt nézd meg az adatbázist!
+            using (Entities entities = new Entities())
+            {
+                Person aranka = entities.Persons.Include(p => p.Addresses).Single(p => p.Id == ArankaId);
+                entities.Addresses.RemoveRange(aranka.Addresses);
+                entities.Persons.Remove(aranka);
+                // Lehetne cascade-delete politikát is felparaméterezni: https://docs.microsoft.com/en-us/ef/core/saving/cascade-delete
+                entities.SaveChanges();
+            }
+            // Itt nézd meg az adatbázist!
         }
     }
 }
