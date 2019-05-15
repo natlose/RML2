b3b3f37f37daab932d949d4b006c305113db9ff4
natlose <natlose@gmail.com>
2019-05-15 16:39:54 +0200

    make use of the context

--- RML_EF/Program.cs
+++ RML_EF/Program.cs
@@ -6,7 +6,14 @@ namespace RML_EF
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (Entities entites = new Entities())
+            {
+                Person person = new Person { FirstName = "Jenő", LastName = "Nagylaki" };
+                person.Addresses.Add(new Address { Kind = AddressKind.MAIL, Country3166 = "HU", Zip = "1043", City = "Budapest", Line1 = "Váci út 103." });
+                entites.Persons.Add(person);
+
+                entites.SaveChanges();
+            }
         }
     }
 }
